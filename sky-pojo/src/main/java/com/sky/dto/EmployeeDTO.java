62186fd9e301277bf53fc1c17d8cb65c2d8aab5b
Mediogen <diogenme@gmail.com>
2025-07-27 22:30:58 +0800

    苍穹外卖day-02

---
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -0,0 +1,28 @@
+package com.sky.dto;
+
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(description = "新增员工数据传输对象")
+public class EmployeeDTO implements Serializable {
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String phone;
+
+    private String sex;
+
+    private String idNumber;
+
+}
